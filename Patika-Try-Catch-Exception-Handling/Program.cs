f30659b7a986801a237c790054f92800880ec3c4
MuhammetCanLikya <158784308+mySmurfChar@users.noreply.github.com>
2024-02-05 20:59:17 +0300

    first commit

---
+++ Patika-Try-Catch-Exception-Handling/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Patika_Try_Catch_Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            try
+            {
+                Console.WriteLine("Bir sayı girinic");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğiniz sayı: "+ sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine ("Hata:"+ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+
+            }*/
+
+            try
+            {
+                Console.WriteLine("Bir değer giriniz.");
+                int a = int.Parse("320000000");
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex.Message);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Girilen değer çok büyük veya çok küçük");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+
+            }
+
+
+
+
+        }
+    }
+}
